b0662ab4d4812c71b92c39feba061c8272075f49
nvegas1230 <nmullin@smail.pembinatrails.ca>
2025-02-06 17:59:38 +0000

    Update for cd actions

--- read-information.py
+++ read-information.py
@@ -5,44 +5,53 @@ main = xmlRead.parse("test.xml")
 root = main.getroot()
 currentPath = root
 running = True
-
-def get_element_names(path):
+def parentLoop(pathGiven):
+  global currentPath
+  check = None
+  for element in pathGiven:
+    if element.tag == currentPath.tag:
+      currentPath = pathGiven
+      break
+    else:
+      parentLoop(element)
+  return check
+def pathUp():
+  global currentPath
+  parentTest = parentLoop(root)
+  if parentTest is not None:
+    elementPath(parentTest)
+def getElementNames(path):
   element_names = ""
-  
   if path is not None:
     for child in path:
       element_names += child.tag + ", "
   element_names+="up"
-  return element_names.strip() 
+  print("Children of element: "+element_names.strip())
 def elementPath(tag):
+  if tag == "up":
+    return 
   if tag is not None:
     newPath = currentPath.find(tag)
-    if newPath is not None:
-      return newPath
-    else:
-      return "failed"
-def getText(path):
-  if path.text is None:
-    return "failed"
-  else:
-    return path.text
+    return newPath
 def action():
-  print("Your current path is: ")
-  print("Actions: exit, text, path")
-  action = input("Input action: ")
-  
+  global currentPath
+  print("\nYour current path is: "+currentPath.tag)
+  print("Actions: exit, cd, ls, gettext")
+  action = input("Input action: ").strip()
+  print()
   if action == "exit":
+    print("Exiting...")
     return action
-
-  if action == "path":
-    options = get_element_names(currentPath)
-    print("options: " + options)
-    tag = input("Input element to go to: ")
-    if tag is None:
-      return
-    elif tag is not None:
-      check = elementPath(tag)
-      print("Path")
+  elif action == "ls":
+    getElementNames(currentPath)
+  elif action == "cd":
+    path = input("Input directory/element: ").strip()
+    if path == "up":
+      pathUp()
+    else:
+      currentPath = elementPath(path)
+  elif action == "gettext":
+    print(currentPath.text)
 while running:
   result = action()
   if result == "exit":
